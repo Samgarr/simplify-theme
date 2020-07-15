ccf263c0201e2e22ca9f6dcd6cb36e6c676ce2a9
Jan Pechek <honza@pechek.cz>
2020-07-15 19:48:51 +0200

    Add new button to project header
    Signed-off-by: Jan Pechek <honza@pechek.cz>

--- Plugin.php
+++ Plugin.php
@@ -7,12 +7,14 @@ class Plugin extends Base
 {
     public function initialize()
     {
+        $this->hook->on('template:layout:css', array('template' => 'plugins/SimplifyTheme/simplifytheme.css'));
         $this->template->setTemplateOverride('board/task_private', 'SimplifyTheme:board/task_private');
         $this->template->setTemplateOverride('board/task_footer', 'SimplifyTheme:board/task_footer');
         $this->template->setTemplateOverride('task/show', 'SimplifyTheme:task/show');
         $this->template->setTemplateOverride('task/sidebar', 'SimplifyTheme:task/sidebar');
         $this->template->setTemplateOverride('task/details', 'SimplifyTheme:task/details');
         $this->template->setTemplateOverride('project_header/views', 'SimplifyTheme:project_header/views');
+        $this->template->setTemplateOverride('project_header/header', 'SimplifyTheme:project_header/header');
         $this->template->setTemplateOverride('task_creation/show', 'SimplifyTheme:task_creation/show');
     }
 
@@ -20,5 +22,4 @@ class Plugin extends Base
     public function getPluginAuthor() {	     return 'Samgarr'; }
     public function getPluginVersion() {     return '0.1'; }
     public function getPluginDescription() { return 'Goal is simplify Kanboard UI and streamline transition from Trello to Kanboard.'; }
-
 }
